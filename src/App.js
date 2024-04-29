5eb9a5ada2ace55c6bc660d06aa67f0c25aa37f1
rp948 <rp948@njit.edu>
2024-04-29 12:35:08 -0400

    shopping-app-init

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='catagories-container'>
+      <div className="catagory-container">
+        {/*{<img/>}*/}
+        <div className="catagory-body-continer">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catagory-container">
+        {/*{<img/>}*/}
+        <div className="catagory-body-continer">
+          <h2>jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catagory-container">
+        {/*{<img/>}*/}
+        <div className="catagory-body-continer">
+          <h2>sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catagory-container">
+        {/*{<img/>}*/}
+        <div className="catagory-body-continer">
+          <h2>womes</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catagory-container">
+        {/*{<img/>}*/}
+        <div className="catagory-body-continer">
+          <h2>mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
