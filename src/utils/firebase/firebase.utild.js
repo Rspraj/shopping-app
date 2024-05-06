787f74142c15e995b4213cbbb9ffc3c9b22af53f
rp948 <rp948@njit.edu>
2024-05-06 11:35:29 -0400

    firebase

---
+++ src/utils/firebase/firebase.utild.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSXR0NcOSGWofJ_SSH8-rI2uw-gVS3XnM",
+  authDomain: "crwn-clothing-db-a88ad.firebaseapp.com",
+  projectId: "crwn-clothing-db-a88ad",
+  storageBucket: "crwn-clothing-db-a88ad.appspot.com",
+  messagingSenderId: "208193547471",
+  appId: "1:208193547471:web:ad8cd46bacad0eff2cdbba"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
